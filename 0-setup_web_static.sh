c7b059bdab1aa6b3441ee27089aa31ccaa5e7345
guillaume <guillaume@holbertonschool.tech>
2017-03-15 16:19:44 -0700

    Task 0: Added install nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Sets up nginx
 #sudo apt-get install nginx
+sudo apt-get update && sudo apt-get install -y nginx
 WEBROOT="/data/web_static"
 sudo mkdir -p $WEBROOT/{releases/test/,shared}
 echo "Holberton School test page" | sudo tee $WEBROOT/releases/test/index.html
@@ -8,3 +9,4 @@ pushd $WEBROOT
 sudo ln -snf releases/test/ current
 popd
 sudo chown -R ubuntu:ubuntu /data/
+sudo service nginx restart
