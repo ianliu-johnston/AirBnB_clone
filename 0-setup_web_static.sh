ce2347018a2aa210561c862df35a483e7e8ff972
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-15 23:15:16 -0700

    Task 0: Forgot part of a path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,12 @@ echo "Holberton School test page" | sudo tee $WEBROOT/releases/test/index.html
 pushd $WEBROOT
 sudo ln -snf releases/test/ current
 popd
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu: /data/
+sudo rm /etc/nginx/sites-enabled/default
+pushd /etc/nginx/sites-enabled/
+ln -snf ../sites-available/default default
+popd
 sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak1
 INSERT=$(grep -n "server_name" /etc/nginx/sites-available/default | cut -d ':' -f1 | head -1)
-sudo sed -i "$INSERT ilocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "$INSERT ilocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
