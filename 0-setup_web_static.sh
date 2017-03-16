d04ba795c1896a7fb837bb21909bb4eb73753b5d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-16 01:12:25 -0700

    Task 0: Fixed text block, so it saves in the right place, with right syntax

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ echo "<DOCTYPE html><head></head><body>Holberton School test page</body></html>"
 pushd $WEBROOT
 sudo ln -snf releases/test/ current
 popd
-sudo chown -R ubuntu: /data/
-echo -ne "user www-data;\nworker_processes 4;\npid /run/nginx.pid;\n\nevents {\n\tworker_connections 768;\n}\n\nhttp {\n\tsendfile on;\n\ttcp_nopush on;\n\ttcp_nodelay on;\n\tkeepalive_timeout 65;\n\ttypes_hash_max_size 2048;\n\tinclude /etc/nginx/mime.types;\n\tdefault_type application/octet-stream;\n\taccess_log /var/log/nginx/access.log;\n\terror_log /var/log/nginx/error.log;\n\tgzip on;\n\tgzip_disable \"msie6\";\n\tinclude /etc/nginx/conf.d/*.conf;\n\tinclude /etc/nginx/sites-enabled/*;\n\tserver {\n\t\tlisten 80;\n\t\tlisten [::]:80 default_server ipv6only=on;\n\n\t\troot /usr/share/nginx/html;\n\t\tindex index.html index.htm;\n\t\trewrite ^/redirect_me/ http://ianxaunliu-johnston.com permanent;\n\t\tserver_name localhost;\n\t\tlocation / {\n\t\t\ttry_files $uri $uri/ =404;\n\t\t}\n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current;\n\t\t}\n\terror_page 404 /404.html\n}" | sudo tee /etc/nginx/nginx.conf > /dev/null 2>&1
+sudo chown -R ubuntu:ubuntu /data/
+echo -ne "server {\n\tlisten 80;\n\troot /usr/share/nginx/html;\n\tindex index.html index.htm;\n\trewrite ^/redirect_me/ http://ianxaunliu-johnston.com permanent;\n\tserver_name localhost;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n\terror_page 404 /404.html;\n}" | sudo tee /etc/nginx/sites-available/default > /dev/null 2>&1
 sudo service nginx restart
