1f10916f23ada16af25342c6c0bfe58794d8960b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-16 01:20:28 -0700

    Task 0: Send all output to /dev/null

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Sets up nginx
-sudo apt-get update && sudo apt-get install -y nginx
+sudo apt-get update > /dev/null 2>&1 && sudo apt-get install -y nginx > /dev/null 2>&1
 WEBROOT="/data/web_static"
-sudo mkdir -p $WEBROOT/{releases/test/,shared}
-echo -ne "<DOCTYPE html><html lang=\"en\">\n<head>\n</head>\n<body>\nHolberton School test page\n</body>\n</html>" | sudo tee $WEBROOT/releases/test/index.html
+sudo mkdir -p $WEBROOT/{releases/test/,shared} > /dev/null 2>&1
+echo -ne "<DOCTYPE html>\n<html lang=\"en\">\n<head>\n</head>\n<body>\nHolberton School test page\n</body>\n</html>" | sudo tee $WEBROOT/releases/test/index.html > /dev/null 2>&1
 pushd $WEBROOT
-sudo ln -snf releases/test/ current
+sudo ln -snf releases/test/ current > /dev/null 2>&1
 popd
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/ > /dev/null 2>&1
 echo -ne "server {\n\tlisten 80;\n\troot /usr/share/nginx/html;\n\tindex index.html index.htm;\n\trewrite ^/redirect_me/ http://ianxaunliu-johnston.com permanent;\n\tserver_name localhost;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n\terror_page 404 /404.html;\n}" | sudo tee /etc/nginx/sites-available/default > /dev/null 2>&1
-sudo service nginx restart
+sudo service nginx restart > /dev/null 2>&1
