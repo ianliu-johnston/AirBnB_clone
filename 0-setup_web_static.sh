fcd9e3f869652ad6619c8a1f1a1c21df1f87055e
guillaume <guillaume@holbertonschool.tech>
2017-03-15 17:23:38 -0700

    Task 0: Forgot terminators

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Sets up nginx
-#sudo apt-get install nginx
 sudo apt-get update && sudo apt-get install -y nginx
 WEBROOT="/data/web_static"
 sudo mkdir -p $WEBROOT/{releases/test/,shared}
@@ -9,4 +8,5 @@ pushd $WEBROOT
 sudo ln -snf releases/test/ current
 popd
 sudo chown -R ubuntu:ubuntu /data/
+sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/releases/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
